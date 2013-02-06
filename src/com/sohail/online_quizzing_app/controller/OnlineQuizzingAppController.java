0f4612bead24bafc55e699abb3e4950ec033fb6a
Sohail Alam <sohail@robozaa.com>
2013-02-06 17:59:28 +0530

    UserLogin FXML Prepared

--- src/com/sohail/online_quizzing_app/controller/OnlineQuizzingAppController.java
+++ src/com/sohail/online_quizzing_app/controller/OnlineQuizzingAppController.java
@@ -9,25 +9,67 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 /**
  *
  * @author sohail.alam
  */
 public class OnlineQuizzingAppController implements Initializable {
-	
+
 	@FXML
-	private Label label;
-	
+	private Button btn_appHelp;
 	@FXML
-	private void handleButtonAction(ActionEvent event) {
-		System.out.println("You clicked me!");
-		label.setText("Hello World!");
-	}
-	
+	private Button btn_appAbout;
+	@FXML
+	private Button btn_appExit;
+	@FXML
+	private Button btn_appLogout;
+	@FXML
+	private Button btn_login;
+	@FXML
+	private Button btn_signup;
+	@FXML
+	private Text txt_headerInfo;
+	@FXML
+	private Text txt_headerUserLoggedInName;
+	@FXML
+	private Text txt_loginMessage;
+	@FXML
+	private Text txt_signupMessage;
+	@FXML
+	private TextField txtbox_loginUsername;
+	@FXML
+	private PasswordField txtbox_loginPassword;
+	@FXML
+	private TextField txtbox_signupEmail;
+	@FXML
+	private TextField txtbox_signupUsername;
+	@FXML
+	private PasswordField txtbox_signupPassword;
+	@FXML
+	private PasswordField txtbox_signupPassword2;
+
+	/**
+	 *
+	 * @param url
+	 * @param rb
+	 */
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		// TODO
-	}	
+	}
+
+	/**
+	 * Exit from the application
+	 *
+	 * @param event
+	 */
+	@FXML
+	private void handleAppExitButtonAction(ActionEvent event) {
+		System.exit(0);
+	}
 }
