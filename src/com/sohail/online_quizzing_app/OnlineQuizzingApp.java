a37cdb81f66678b0e07f4733ebf798ab51e1879f
Sohail Alam <sa4250mnpo70@yahoo.com>
2013-02-07 23:58:14 +0530

    Dialog box added requesting for Full Screen mode.
    Fixed issue of scene height/width while switching between scenes.

--- src/com/sohail/online_quizzing_app/OnlineQuizzingApp.java
+++ src/com/sohail/online_quizzing_app/OnlineQuizzingApp.java
@@ -5,6 +5,9 @@
 package com.sohail.online_quizzing_app;
 
 import javafx.application.Application;
+import javafx.scene.control.Dialogs;
+import javafx.scene.control.Dialogs.DialogOptions;
+import javafx.scene.control.Dialogs.DialogResponse;
 import javafx.stage.Stage;
 
 /**
@@ -13,59 +16,71 @@ import javafx.stage.Stage;
  */
 public class OnlineQuizzingApp extends Application {
 
-	private static boolean isLoggedIn = false;
-	private static OnlineQuizzingApp instance;
-	private Stage stage;
+    private static boolean isLoggedIn = false;
+    private static OnlineQuizzingApp instance;
+    private Stage stage;
 
-	public OnlineQuizzingApp() {
-		instance = this;
-	}
+    public OnlineQuizzingApp() {
+        instance = this;
+    }
 
-	public static OnlineQuizzingApp getInstance() {
-		return instance;
-	}
+    public static OnlineQuizzingApp getInstance() {
+        return instance;
+    }
 
-	/**
-	 * The main() method is ignored in correctly deployed JavaFX
-	 * application. main() serves only as fallback in case the application
-	 * can not be launched through deployment artifacts, e.g., in IDEs with
-	 * limited FX support. NetBeans ignores main().
-	 *
-	 * @param args the command line arguments
-	 */
-	public static void main(String[] args) {
-		launch(args);
-	}
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		try {
-			stage = primaryStage;
-			SceneLoaders.getInstance().gotoLogin(getStage());
-			primaryStage.show();
-		} catch (Exception ex) {
-			//Handle Exception
-		}
-	}
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        try {
+            stage = primaryStage;
+            SceneLoaders.getInstance().gotoLogin(getStage());
 
-	/**
-	 * @return the isLoggedIn
-	 */
-	public static boolean isIsLoggedIn() {
-		return isLoggedIn;
-	}
+            //Show dialog requesting full screen mode
+            DialogResponse response = Dialogs.showConfirmDialog(stage,
+                    "Do you want to continue with Full Screen Enabled. You may also switch between Full Screen and Normal Screen later.",
+                    "For a better visual experience, it is recommended to run this application in Full Screen Mode.",
+                    "Full Screen Access", DialogOptions.YES_NO);
+            if (response.equals(response.YES)) {
+                SceneLoaders.getInstance().doFullScreen(true, stage);
+            } else {
+                SceneLoaders.getInstance().doFullScreen(false, stage);
+            }
+            
+            primaryStage.show();
+        } catch (Exception ex) {
+            //Handle Exception
+        }
+    }
 
-	/**
-	 * @param isLoggedIn the isLoggedIn to set
-	 */
-	public static void setIsLoggedIn(boolean isLoggedIn) {
-		OnlineQuizzingApp.isLoggedIn = isLoggedIn;
-	}
+    /**
+     * @return the isLoggedIn
+     */
+    public static boolean isIsLoggedIn() {
+        return isLoggedIn;
+    }
 
-	/**
-	 * @return the stage
-	 */
-	public Stage getStage() {
-		return stage;
-	}
+    /**
+     * @param isLoggedIn the isLoggedIn to set
+     */
+    public static void setIsLoggedIn(boolean isLoggedIn) {
+        OnlineQuizzingApp.isLoggedIn = isLoggedIn;
+    }
+
+    /**
+     * @return the stage
+     */
+    public Stage getStage() {
+        return stage;
+    }
 }
--- src/com/sohail/online_quizzing_app/SceneLoaders.java
+++ src/com/sohail/online_quizzing_app/SceneLoaders.java
@@ -14,114 +14,122 @@ import javafx.stage.Stage;
  *
  * @author sohail.alam
  */
-public class SceneLoaders implements AllScenesInterface{
+public class SceneLoaders implements AllScenesInterface {
 
-	/*
-	 * Making it a singleton class
-	 */
-	private static SceneLoaders instance = new SceneLoaders();
+    /*
+     * Making it a singleton class
+     */
+    private static SceneLoaders instance = new SceneLoaders();
 
-	private SceneLoaders() {
-	}
+    private SceneLoaders() {
+    }
 
-	public static SceneLoaders getInstance() {
-		return instance;
-	}
+    public static SceneLoaders getInstance() {
+        return instance;
+    }
 
-	/*
-	 * This is the Login Scene
-	 */
-	synchronized public void gotoLogin(Stage stage) {
-		try {
-			replaceSceneContent(ONLINE_QUIZZING_APP_FXML, ONLINE_QUIZZING_APP_CSS, stage);
-		} catch (Exception ex) {
-			//Handle Exception
-			ex.printStackTrace();
-		}
-	}
+    /*
+     * This is the Login Scene
+     */
+    synchronized public void gotoLogin(Stage stage) {
+        try {
+            replaceSceneContent(ONLINE_QUIZZING_APP_FXML, ONLINE_QUIZZING_APP_CSS, stage);
+        } catch (Exception ex) {
+            //Handle Exception
+            ex.printStackTrace();
+        }
+    }
 
-	/*
-	 * Admin Home Scene
-	 */
-	synchronized public void gotoAdminHome(Stage stage) {
-		try {
-			replaceSceneContent(ADMIN_HOME_FXML, ADMIN_HOME_CSS, stage);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    /*
+     * Admin Home Scene
+     */
+    synchronized public void gotoAdminHome(Stage stage) {
+        try {
+            replaceSceneContent(ADMIN_HOME_FXML, ADMIN_HOME_CSS, stage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	/*
-	 * Admin User Management Scene
-	 */
-	synchronized public void gotoAdminManageUsers(Stage stage) {
-		try {
-			replaceSceneContent(ADMIN_MANAGE_USERS_FXML, ADMIN_MANAGE_USERS_CSS, stage);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/*
-	 * Admin User Management Scene
-	 */
-	synchronized public void gotoAdminManageQuiz(Stage stage) {
-		try {
-			replaceSceneContent(ADMIN_MANAGE_QUIZ_FXML, ADMIN_MANAGE_QUIZ_CSS, stage);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
-	/*
-	 * Admin User Management Scene
-	 */
-	synchronized public void gotoAdminViewQuiz(Stage stage) {
-		try {
-			replaceSceneContent(ADMIN_VIEW_QUIZ_FXML, ADMIN_VIEW_QUIZ_CSS, stage);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
-	/*
-	 * Admin User Management Scene
-	 */
-	synchronized public void gotoAdminViewScores(Stage stage) {
-		try {
-			replaceSceneContent(ADMIN_VIEW_SCORES_FXML, ADMIN_VIEW_SCORES_CSS, stage);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    /*
+     * Admin User Management Scene
+     */
+    synchronized public void gotoAdminManageUsers(Stage stage) {
+        try {
+            replaceSceneContent(ADMIN_MANAGE_USERS_FXML, ADMIN_MANAGE_USERS_CSS, stage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	/**
-	 * This method replaces one scene with another.
-	 * No need to call it explicitely.
-	 *
-	 * @param fxml The relative path to the fxml file needed to be loaded
-	 * @param stylesheet The relative path to the stylesheet needed to be loaded
-	 * @param stage The stage to which the scene needs to be loaded
-	 * @return
-	 * @throws Exception
-	 */
-	synchronized public Parent replaceSceneContent(String fxml, String stylesheet, Stage stage) throws Exception {
-		
-		double stageWidth = stage.getWidth();
-		double stageHeight = stage.getHeight();
-		
-		Parent page = (Parent) FXMLLoader.load(OnlineQuizzingApp.class.getResource(fxml), null, new JavaFXBuilderFactory());
-		Scene scene = stage.getScene();
-		if (scene == null) {
-			scene = new Scene(page, stageWidth, stageHeight);
-			scene.getStylesheets().add(OnlineQuizzingApp.class.getResource(stylesheet).toExternalForm());
-			stage.setScene(scene);
-		} else {
-			stage.getScene().setRoot(page);
-		}
-		stage.sizeToScene();
-		return page;
-	}
+    /*
+     * Admin User Management Scene
+     */
+    synchronized public void gotoAdminManageQuiz(Stage stage) {
+        try {
+            replaceSceneContent(ADMIN_MANAGE_QUIZ_FXML, ADMIN_MANAGE_QUIZ_CSS, stage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+     * Admin User Management Scene
+     */
+    synchronized public void gotoAdminViewQuiz(Stage stage) {
+        try {
+            replaceSceneContent(ADMIN_VIEW_QUIZ_FXML, ADMIN_VIEW_QUIZ_CSS, stage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+     * Admin User Management Scene
+     */
+    synchronized public void gotoAdminViewScores(Stage stage) {
+        try {
+            replaceSceneContent(ADMIN_VIEW_SCORES_FXML, ADMIN_VIEW_SCORES_CSS, stage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * This method replaces one scene with another. No need to call it
+     * explicitely.
+     *
+     * @param fxml The relative path to the fxml file needed to be loaded
+     * @param stylesheet The relative path to the stylesheet needed to be loaded
+     * @param stage The stage to which the scene needs to be loaded
+     * @return
+     * @throws Exception
+     */
+    synchronized public Parent replaceSceneContent(String fxml, String stylesheet, Stage stage) throws Exception {
+
+        double stageWidth = stage.getWidth();
+        double stageHeight = stage.getHeight();
+
+        Parent page = (Parent) FXMLLoader.load(OnlineQuizzingApp.class.getResource(fxml), null, new JavaFXBuilderFactory());
+        Scene scene = stage.getScene();
+        if (scene == null) {
+            scene = new Scene(page, stageWidth, stageHeight);
+            scene.getStylesheets().add(OnlineQuizzingApp.class.getResource(stylesheet).toExternalForm());
+            stage.setScene(scene);
+        } else {
+            stage.getScene().setRoot(page);
+        }
+        //stage.sizeToScene();
+        return page;
+    }
+    
+    /*
+     * Use this method to switch between Full Screen Mode and Normal Screen Mode.
+     */
+    synchronized public void doFullScreen(boolean doFullScreen, Stage stage) {
+        try {
+            stage.setFullScreen(doFullScreen);
+        } catch (Exception e) {
+        }
+    }
 }
