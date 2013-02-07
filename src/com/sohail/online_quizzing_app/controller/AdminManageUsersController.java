0afc30e1898ee7589e4bca5da8a8646e340bc050
Sohail Alam <sa4250mnpo70@yahoo.com>
2013-02-07 09:12:52 +0530

    New View and Controller for Admin Users

---
+++ src/com/sohail/online_quizzing_app/controller/AdminManageUsersController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sohail.online_quizzing_app.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author SOHAIL
+ */
+public class AdminManageUsersController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
